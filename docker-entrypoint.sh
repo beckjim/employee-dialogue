3f3a6f99e7021587ba245565aea0a4c14b533579
Stefan Beck (ext.) <Stefan.Beck@partner.bmw.de>
2026-08-12 15:48:42 +0200

    feat(services): update nginx configuration and remove deprecated entrypoint script

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-python -c "import employee_dialogue"
-export SKIP_DB_INIT=1
-
-gunicorn --bind 0.0.0.0:5000 --workers 2 --worker-class sync --timeout 120 --access-logfile - --error-logfile - "employee_dialogue:app" &
-gunicorn --bind 0.0.0.0:443 --certfile certs/cert.pem --keyfile certs/key.pem --workers 2 --worker-class sync --timeout 120 --access-logfile - --error-logfile - "employee_dialogue:app"
